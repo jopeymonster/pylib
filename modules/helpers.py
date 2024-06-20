d0119aa1d5d206c649212e6e2885f162af17f7c5
Joe Thompson <jopeymonster83@gmail.com>
2024-06-20 14:39:38 -0700

    Restructured Python folder, scoped this repo to pylib only

--- modules/helpers.py
+++ modules/helpers.py
@@ -233,3 +233,37 @@ value_to_assign = 42
 variables[var_name] = f"'{value_to_assign}'"
 # Access the variable
 print(variables["dynamic_var"])  # Output: '42'
+
+
+"""capture parameters"""
+import re
+def match_url(url):
+  """Matches the given url against the regex."""
+  match = re.match(r'\/\?(ccode|pcode)=(.*)', url)
+  if match:
+    return match.groups()
+  else:
+    return None
+
+"""example"""
+if __name__ == '__main__':
+  url = '/?ccode=12345&pcode=54321'
+  ccode, pcode = match_url(url)
+  print(ccode, pcode)
+
+
+"""regex target page capture parameter"""
+import re
+def match_url(url):
+  """Matches the given url against the regex."""
+  match = re.match(r'\/productvideo\.html\?pcode=(.*)|\/categoryvideo\.html\?pcode=(.*)', url)
+  if match:
+    return match.group(1)
+  else:
+    return None
+
+"""example"""
+if __name__ == '__main__':
+  url = '/productvideo.html?pcode=12345'
+  pcode = match_url(url)
+  print(pcode)
